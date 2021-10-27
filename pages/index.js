91db9f90e70731d2bf1367ee76e3f88ace27dd2e
Samrat Pudasaini <pudasaini.samrat@gmail.com>
2021-10-27 10:29:18 +0545

    Banner section completed

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import Head from "next/head"
 import Header from "../components/Header"
+import Banner from "../components/Banner"
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <Banner />
     </div>
   )
 }
