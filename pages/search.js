fdff5d854ecf2f0f55ec80253366ce32072fcb58
Samrat Pudasaini <pudasaini.samrat@gmail.com>
2021-11-16 21:21:02 +0545

    Loading bar implemented

--- pages/search.js
+++ pages/search.js
@@ -3,9 +3,12 @@ import Header from "../components/Header"
 import Footer from "../components/Footer"
 import { useRouter } from "next/dist/client/router"
 import { format } from "date-fns"
+import InfoCard from "../components/InfoCard"
 
-const Search = () => {
+const Search = ({ searchResults }) => {
   const router = useRouter()
+
+  // console.log(searchResults)
 
   const { location, startDate, endDate, noOfGuests } = router.query
 
@@ -33,6 +36,22 @@ const Search = () => {
             <p className="button">Rooms and Beds</p>
             <p className="button">More filters</p>
           </div>
+
+          <div className="flex flex-col">
+            {searchResults.map(
+              ({ img, location, title, description, star, price, total }) => (
+                <InfoCard
+                  img={img}
+                  location={location}
+                  title={title}
+                  description={description}
+                  star={star}
+                  price={price}
+                  total={total}
+                />
+              )
+            )}
+          </div>
         </section>
       </main>
 
@@ -42,3 +61,15 @@ const Search = () => {
 }
 
 export default Search
+
+export async function getServerSideProps() {
+  const searchResults = await fetch("https://links.papareact.com/isz").then(
+    (res) => res.json()
+  )
+
+  return {
+    props: {
+      searchResults,
+    },
+  }
+}
